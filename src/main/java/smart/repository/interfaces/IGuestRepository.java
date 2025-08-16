ebd1722d404374eae94230074cc8aed3fce1a1bc
Husăsan Smaranda-Ioana <smara.husasan@gmail.com>
2025-08-16 14:28:40 +0300

    added getGuestsForHotel

--- src/main/java/smart/repository/interfaces/IGuestRepository.java
+++ src/main/java/smart/repository/interfaces/IGuestRepository.java
@@ -2,6 +2,9 @@ package smart.repository.interfaces;
 
 import smart.domain.Guest;
 
+import java.util.List;
+
 public interface IGuestRepository {
     void addGuest(Guest guest);
+    List<Guest> getGuestsForHotel(int hotelId);
 }
