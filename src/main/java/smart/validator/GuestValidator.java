c89dd5459d4ff8b9050383e5b7b7db3c423d5a58
Husăsan Smaranda-Ioana <smara.husasan@gmail.com>
2025-08-17 21:08:34 +0300

    corrected guest validation

--- src/main/java/smart/validator/GuestValidator.java
+++ src/main/java/smart/validator/GuestValidator.java
@@ -6,9 +6,9 @@ import smart.utils.exceptions.ValidationException;
 public class GuestValidator implements Validator<Guest>{
     @Override
     public void validate(Guest entity) throws ValidationException {
-        if(entity.getName().isEmpty())
-            throw new ValidationException("Guest name is required");
-        if(entity.getEmail().isEmpty() && !entity.getEmail().matches("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}$"))
+        if(entity.getName().isEmpty() || !entity.getName().matches("^[a-zA-Z ]+$"))
+            throw new ValidationException("Guest name is invalid");
+        if(entity.getEmail().isEmpty() || !entity.getEmail().matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"))
             throw new ValidationException("Guest email is invalid");
         if(entity.getPhone().isEmpty() || !entity.getPhone().matches("^[0-9]+$"))
             throw new ValidationException("Guest phone number is invalid");
