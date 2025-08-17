0aebdfd81bfa7fe4db7b5ae7cf866228d0d53f69
Husăsan Smaranda-Ioana <smara.husasan@gmail.com>
2025-08-18 02:28:47 +0300

    created methods to eliminate duplicate code

--- src/main/java/smart/utils/Input.java
+++ src/main/java/smart/utils/Input.java
@@ -1,7 +1,35 @@
 package smart.utils;
 
+import java.sql.Date;
 import java.util.Scanner;
 
 public class Input {
     public static final Scanner sc = new Scanner(System.in);
+
+    public static int getIntFromInput(){
+        int id;
+        while (true) {
+            try {
+                String line=Input.sc.nextLine();
+                id = Integer.parseInt(line);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid data. Please try again.");
+            }
+        }
+        return id;
+    }
+
+    public static Date getDateFromInput(){
+        Date date;
+        while(true){
+            try{
+                date = Date.valueOf(Input.sc.nextLine().trim());
+                break;
+            }catch (IllegalArgumentException e){
+                System.out.println("Invalid data. Please try again.");
+            }
+        }
+        return date;
+    }
 }
