e89e7f79c5dbcbeed43088fc948a4c1bc78c44b6
Husăsan Smaranda-Ioana <smara.husasan@gmail.com>
2025-09-02 14:04:45 +0300

    made Guest a record class

--- src/main/java/smart/domain/Guest.java
+++ src/main/java/smart/domain/Guest.java
@@ -1,60 +1,6 @@
 package smart.domain;
 
-public class Guest {
-    private int guestId;
-    private String email;
-    private String name;
-    private String phone;
-    private int hotelId;
-
-    public Guest(int guestId, String email, String name, String phone, int hotelId) {
-        this.guestId = guestId;
-        this.email = email;
-        this.name = name;
-        this.phone = phone;
-        this.hotelId = hotelId;
-    }
-
-    public int getGuestId() {
-        return guestId;
-    }
-
-    public void setGuestId(int guestId) {
-        this.guestId = guestId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public int getHotelId() {
-        return hotelId;
-    }
-
-    public void setHotelId(int hotelId) {
-        this.hotelId = hotelId;
-    }
-
+public record Guest(int guestId,String email,String name, String phone, int hotelId) {
     @Override
     public String toString() {
         return "Guest{" +
